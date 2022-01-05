b671dfa9dd57710226561ac67c84c6e83164599e
heyikhwan <ikhwanmail231000@gmail.com>
2022-01-05 13:28:17 +0700

    'finish'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { increment, decrement, signIn } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Belajar React Redux</h1>
+      <p>Ikhwan Studio</p>
+
+      <br />
+
+      <h3>Counter: {counter}</h3>
+
+      <div className="tombol">
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(increment())}>+</button>
+      </div>
+
+      <br />
+      <br />
+
+      <p>
+        User dalam keadaan{" "}
+        <strong>{isLogged ? "Sudah Login" : "Belum Login"}</strong>
+      </p>
+      <br />
+      <button onClick={() => dispatch(signIn())}>
+        {isLogged ? "Keluar" : "Masuk"}
+      </button>
     </div>
   );
 }
